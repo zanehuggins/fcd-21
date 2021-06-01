c4132faee48b1df73225205ddb672f7ad5f75061
Zane Huggins <zahuggins@mix.wvu.edu>
2021-06-01 08:59:01 -0400

    .

---
+++ js/index.js
@@ -0,0 +1,12 @@
+Result Skip Results Iframe
+EDIT ON
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
+
+var animation = lottieWeb.loadAnimation({
+  container: document.getElementById('container'),
+  path: 'data.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  name: "Demo Animation",
+});
